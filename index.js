f1e3711f7df7f3e6d1b516badf13c56a0c58a8ad
Maureen Nyanamba <maureenmoraa006@gmail.com>
2024-06-28 19:57:33 +0300

    Modify index.js

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Write your solution in this file!
+const employee ={
+    name: "Maureen",
+    streetAddress: "87-Ndumbuini Road",
+};
+
+function updateEmployeeWithKeyAndValue (employee, key, value){
+return {
+    ...employee,
+    [key]: value,
+};
+};
+
+function destructivelyUpdateEmployeeWithKeyAndValue (employee, key, value){
+    employee[key] = value;
+    return employee;
+};
+
+const deleteFromEmployeeByKey = (employee, key) => {
+    const updatedEmployeeDetail = { ...employee};
+    delete updatedEmployeeDetail[key];
+    return updatedEmployeeDetail;
+};
+
+function destructivelyDeleteFromEmployeeByKey (employee, key){
+    delete employee[key];
+    return employee;
+};
